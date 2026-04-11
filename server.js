7ef8770dfe5c7bde243e19800b268f974048e370
Kshaunish Gupta <kshaunishgupta1@gmail.com>
2026-04-11 16:26:57 +0530

    mongodb fixed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const session = require("express-session");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
